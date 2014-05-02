13abd9b05e6104cf6dca6bb5e74c00a8b76ef180
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-05-02 12:05:24 +1000

    Add new layout files - WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ WithGusto::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+# turn off static asset serving
+  config.serve_static_assets = true
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
