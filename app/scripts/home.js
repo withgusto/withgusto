63b82e6e8589d930dac183c054caf69aab5f348a
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-05-07 15:44:31 +1000

    Added html5 push state, scroll to section, show elements

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,6 +1,3 @@
-////= require require-js
-//= require foundation.min
-//= require packery/packery
 
 function appendRandomSizedItems( container ) {
   var frag = document.createDocumentFragment();
@@ -25,7 +22,7 @@ var $container = $('.container');
 
   var $firstImage = $('#firstImage');
 
-  var ex4 = document.getElementById('ex4');
+  var ex4 = document.getElementById('work');
   var packery = new Packery( ex4 ,{gutter:20, transitionDuration:'0s'});
 
   packery.on( 'layoutComplete', function() {
@@ -35,11 +32,78 @@ var $container = $('.container');
   } );
 
   packery.layout();
-  Foundation.init();
+
+  handleResize($( window ).width(), $( window ).height() );
+
+  $(document).foundation();
+
+  $( window ).resize(function() {
+    handleResize($( window ).width(), $( window ).height() );
+  });
+
+  showElements();
+  // initialisePushState();
+
+  $(document).on('click', 'a', function (event) {
+
+    event.preventDefault();
+
+    var targetID = event.target.href.split('#')[1];
+
+    setPushState( {title:targetID, href:targetID} );
+
+    scrollToSection( targetID );
+
+  });
 
 })();
+
+function scrollToSection(section) {
+  var diff = 100;
+  if( section === 'work' ){  diff = 0; }
+
+  $( '#' + section).velocity("scroll", { duration:400, ease:'easeOutQuad', offset:-diff } );
+}
+
+function showElements() {
+  $('.intro').css( { 'visibility': 'visible'} );
+  $('.about').css( { 'visibility': 'visible'} );
+}
+
+function handleResize(w, h) {
+
+  var centerValue = ( (h / 2) - 140 )+ 'px';
+
+  $('.intro').css( { 'height': (h ) + 'px' , 'padding-top': centerValue } );
+  $('.about').css( { 'height': (h ) + 'px' , 'padding-top': centerValue } );
+}
 
 function addItem(data) {
   var contentString = '<div id=' + data.id + ' class="item w4 h4" style="background-position: center center; background-image: url(' + data.image + ');"><h1>Hello World</h1></div>';
   $container.prepend(contentString);
 }
+
+function initialisePushState() {
+
+  if (typeof history.pushState === 'undefined') {
+    console.log('fail');
+  }
+  else {
+    console.log('HTML5 History API available');
+  }
+
+}
+
+function setPushState(state) {
+   history.pushState(state.title, state.title, state.href);
+}
+
+addEvent(window, 'popstate', function (event) {
+  var data = event.state;
+  console.log('popstate', event);
+  console.log(event.state || { url: "unknown", name: "undefined", location: "undefined" });
+});
+
+addEvent(window, 'hashchange', function (event) {
+  console.log('hashchange', event);
+});
