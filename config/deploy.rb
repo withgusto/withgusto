cfbcfe4c658ada389bc7ee0961c8787dde135d84
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-05-11 13:35:10 +1000

    Update to deploy with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,6 @@
+# config/deploy.rb
+require 'capistrano/s3'
+
+set :bucket, "withgusto.co"
+set :access_key_id, "AKIAJDV7QI22RTN7F77A"
+set :secret_access_key, "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"
