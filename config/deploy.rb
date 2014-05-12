837b02f55e19cf1ee908e6c2ff48d6d33b26c5e8
Shaun O'Connor <shaun@shaun-oconnor.com>
2014-05-12 12:12:21 +1000

    Update credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@
 require 'capistrano/s3'
 
 set :bucket, "withgusto.co"
-set :access_key_id, "AKIAJDV7QI22RTN7F77A"
-set :secret_access_key, "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"
+set :access_key_id, (ENV['AWS_ACCESS_KEY_ID'])
+set :secret_access_key, "(ENV['AWS_SECRET_ACCESS_KEY'])
